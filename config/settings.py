b610f57997e92b4c696ad374c93c9d9c2b0aec7f
petrakiio <eup0022@gmail.com>
2026-02-25 15:13:16 -0500

    Concertando index

--- config/settings.py
+++ config/settings.py
@@ -114,4 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
